54d26ae07e0e773837b7ae6d6cebcb635137adca
AnthonyRuffino <aruffino84@gmail.com>
2022-10-14 14:01:41 +0200

    pre-commit hook to build code.

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,6 @@
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save --quiet --keep-index --include-untracked $STASH_NAME
 
-# Test prospective commit
-...
-
 STASHES=$(git stash list)
 if [[ $STASHES == *"$STASH_NAME" ]]; then
   git stash pop --quiet
